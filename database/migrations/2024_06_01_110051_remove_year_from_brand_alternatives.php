4238162e211f3cb53cdf20a6ab4a8104e45b76a2
montagbadil <alternativebrands86@gmail.com>
2024-06-01 14:09:38 +0300

    remove year from migration

--- database/migrations/2024_06_01_110051_remove_year_from_brand_alternatives.php
+++ database/migrations/2024_06_01_110051_remove_year_from_brand_alternatives.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('brand_alternatives', function (Blueprint $table) {
-            //
+            Schema::dropColumn('year');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('brand_alternatives', function (Blueprint $table) {
-            Schema::dropColumn('year');
+            
         });
     }
 };
