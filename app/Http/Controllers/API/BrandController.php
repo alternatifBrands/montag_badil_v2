bdcc611ec43184cda6f6f15fbdf212dc4cb0eb47
Abdo3297 <abdelrhmanhassanmohamed@gmail.com>
2024-06-29 14:46:28 +0300

    remove locations

--- app/Http/Controllers/API/BrandController.php
+++ app/Http/Controllers/API/BrandController.php
@@ -18,13 +18,8 @@ class BrandController extends Controller
     {
         $countryName = $request->query('country');
         $countryId = $request->query('location_id');
-        $query = Brand::where('status', 'approved')->whereHas('locations', function ($query) use ($countryId) {
-            if ($countryId) {
-                $query->where('country_id', $countryId);
-            }
-        })->with([
+        $query = Brand::where('status', 'approved')->with([
             'user',
-            'locations',
             'category',
             'country',
             'city',
