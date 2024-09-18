f75462781402ea5e36ff59b76c7999249ed92242
Frosia-tech <safya@dal-digital.com>
2024-09-18 12:47:08 +0300

    settigs update

--- app/Http/Controllers/API/SettingsController.php
+++ app/Http/Controllers/API/SettingsController.php
@@ -10,9 +10,31 @@ use App\Http\Resources\API\SettingResource;
 
 class SettingsController extends Controller
 {
+    protected function isJson($string): bool
+    {
+        if (!is_string($string)) {
+            return false;
+        }
+
+        json_decode($string);
+        return json_last_error() === JSON_ERROR_NONE;
+    }
     public function index(): JsonResponse
     {
         $settings = Setting::select('key','value')->get();
-        return response()->json(SettingResource::collection($settings));
+        $data = [];
+        foreach($settings as $setting){
+            
+            $fileInputKeys = ['logo', 'favicon', 'hero_image']; 
+            $defaultImage = asset('default/profile.jpeg');
+            if (in_array($setting->key, $fileInputKeys)) {
+                $data[$setting->key] =  $setting->value ? asset('storage/' . $setting->value) : $defaultImage;
+            } elseif ($this->isJson($setting->value)) {
+                $data[$setting->key] =  json_decode($setting->value, true);
+            } else {
+                $data[$setting->key] =  $setting->value;
+            }
+        }
+        return response()->json($data);
     }
 }
